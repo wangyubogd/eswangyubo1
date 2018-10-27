0cad7f83318e92928f31325b8547fdff1d1bcf20
初始 <https://github.com/wangyubogd/eswangyubo1.git>
2018-10-27 23:53:31 +0800

    meituan

--- meituan/src/components/Home.js
+++ meituan/src/components/Home.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import { NavBar, Icon, SearchBar, Grid} from 'antd-mobile';
+import { NavBar, Icon, SearchBar, Grid, Flex, WhiteSpace} from 'antd-mobile';
 import './../asset/css/home.css';
-
+import Container from './Container';
+import Product from './Product';
+import 'whatwg-fetch';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     constructor(props) {
@@ -65,29 +68,65 @@ export default class Home extends Component {
                     text:"电影"
                 }
             ]
+            ,
+            product:[]
         }
+    }
+
+    componentWillMount(){
+        fetch('http://www.xiechenxi.cn/')
+            .then((response) => {
+                return response.json()
+            }).then((data) => {
+                this.setState({
+                    product:data
+                })
+                console.log(data)
+            })
     }
     
     render() {
-    
-    return (
-        <div>
-            <NavBar
-                mode="light"
-                leftContent="哈尔滨"
-                rightContent={[
-                    <Icon key="1" type="ellipsis" />,
-                ]}
-                >
-                <SearchBar placeholder="输入搜索内容" maxLength={8} />
-            </NavBar>
-            <Grid data={this.state.data} isCarousel columnNum={5} />
+        return (
+            <div>
+                <NavBar
+                    mode="light"
+                    leftContent="哈尔滨"
+                    rightContent={[
+                        <Icon key="1" type="ellipsis" />,
+                    ]}
+                    >
+                    <SearchBar placeholder="输入搜索内容" maxLength={8} />
+                </NavBar>
+                <Grid itemStyle={{height:'100px'}} data={this.state.data} isCarousel columnNum={5} hasLine={false} />
+                <WhiteSpace size="lg"></WhiteSpace>
+                <Flex>
+                    <Flex.Item>
+                        <h3 className="active-title">我们约会吧</h3>
+                        <p className="active-text">恋人家人好朋友</p>
+                        <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                    </Flex.Item>
+                    <Flex.Item>
+                    <h3 className="active-title">我们约会吧</h3>
+                        <p className="active-text">恋人家人好朋友</p>
+                        <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                    </Flex.Item>
+                    <Flex.Item>
+                    <h3 className="active-title">我们约会吧</h3>
+                        <p className="active-text">恋人家人好朋友</p>
+                        <img className="active-img" src={require("./../asset/img/activity1.png")}></img>
+                    </Flex.Item>
+                </Flex>
+                <WhiteSpace size="lg"></WhiteSpace>
+                <Container title="猜你喜欢" >
+                    {
+                        this.state.product.map((elem,index)=>{
+                            // return <Link key={elem.product_id}  to="/detail"><Product elem={elem}></Product></Link>
+                            return <Product key={elem.product_id} elem={elem}></Product>
+                        })
+                    }
+                </Container>
 
-            
-
-            
-            
-        </div>
-    )
+            </div>
+        )
   }
 }
